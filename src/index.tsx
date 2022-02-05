70e3ac73c9c1d130652e2a4ea831b1d72c846df9
Guilherme Vieira <hello@guilhermevieira.dev>
2022-02-05 19:44:17 -0300

    feat: configure miragejs and axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          description: 'Desenvolvimento de Website',
+          amount: 5000,
+          type: 'Incoming',
+          category: 'Freelance',
+          date: '20/06/2020'
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
