591767db1eb4aa33229a862d00438560eae4cb6b
Guilherme Vieira <hello@guilhermevieira.dev>
2022-02-06 15:54:58 -0300

    feat: create context and useTransaction hook

--- src/components/Dashboard/Summary/index.tsx
+++ src/components/Dashboard/Summary/index.tsx
@@ -1,10 +1,26 @@
 import { Container } from "./styles";
-
 import incomeImg from "../../../assets/income.svg";
 import outcomeImg from "../../../assets/outcome.svg";
 import totalImg from "../../../assets/total.svg";
+import { useTransactions } from "../../../hooks/useTransactions";
 
 export function Summary() {
+
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce((acc, transaction) => {
+
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc
+  }, { deposits: 0, withdraws: 0, total: 0 })
+
   return (
     <Container>
       <div>
@@ -13,7 +29,10 @@ export function Summary() {
           <img src={incomeImg} alt="incomings" />
         </header>
         <strong>
-          R$1000
+          { new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits) }
         </strong>
       </div>
 
@@ -23,7 +42,10 @@ export function Summary() {
           <img src={outcomeImg} alt="outcome" />
         </header>
         <strong>
-          R$500
+         - { new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws) }
         </strong>
       </div>
 
@@ -33,7 +55,10 @@ export function Summary() {
           <img src={totalImg} alt="Total" />
         </header>
         <strong>
-          R$500
+        { new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total) }
         </strong>
       </div>
     </Container>
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
 import Modal from 'react-modal';
+import { FormEvent, useState } from 'react';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,18 +12,26 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const { createTransaction } = useTransactions();
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    const data = {
-      title, value, category, type
-    }
-
-    api.post('/transactions', data);
+    
+    await createTransaction({
+      title,
+      amount,
+      type,
+      category
+    });
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
   }
 
   return (
@@ -39,7 +47,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Container onSubmit={handleCreateNewTransaction}>
       <h2>Add transaction</h2>
       <input value={title} onChange={event => setTitle(event.target.value)} type="text" id="title" placeholder="Title"/>
-      <input value={value} onChange={event => setValue(Number(event.target.value))} type="number" id="value" placeholder="Value"/>
+      <input value={amount} onChange={event => setAmount(Number(event.target.value))} type="number" id="value" placeholder="Value"/>
       <TransactionTypeContainer>
         <RadioBox
           type="button"
